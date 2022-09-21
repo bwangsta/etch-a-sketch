490aa9c88a755be143387c08b949a96cfdf55003
Brian Wang <bwbrianwang1018@gmail.com>
2022-09-21 16:50:53 -0700

    Add new feature to reset grid and display a different size

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-const GRID_WIDTH = 16;
-const GRID_HEIGHT = 16;
-
 // Create a div using Javascript
 // Add a class called squares to the new div
 // Append it to the container div
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
 
-function createGrid() {
-    for (let i = 0; i < GRID_WIDTH; i++) {
-        for (let j = 0; j < GRID_HEIGHT; j++) {
-            const square = document.createElement("div");
-            square.classList.add("square");
-            container.appendChild(square);
-        }
+createGrid(16);
+draw();
+
+button.addEventListener("click", () => {
+    let gridSize = Number(window.prompt("Choose the size of the grid"));
+    // Only creates new grid if user enters valid input or does not cancel
+    if (gridSize > 0) {
+        resetGrid();
+        createGrid(gridSize);
+        draw();
     }
-}
+});
 
 function draw() {
     const squares = document.querySelectorAll(".square");
@@ -25,17 +26,27 @@ function draw() {
     });
 }
 
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            container.appendChild(square);
+        }
+    }
+
+    container.style.gridTemplateColumns = `repeat(${size}, auto)`;
+    container.style.gridTemplateRows = `repeat(${size}, auto)`;
+}
+
+function resetGrid() {
+    container.textContent = "";
+}
+
 function selectRandomColor() {
-    let red = Math.random() * 255;
-    let green = Math.random() * 255;
-    let blue = Math.random() * 255;
+    let red = Math.random() * 256;
+    let green = Math.random() * 256;
+    let blue = Math.random() * 256;
 
     return `rgb(${red}, ${green}, ${blue})`;
 }
-
-function main() {
-    createGrid();
-    draw();
-}
-
-main();
