4be216454c9ec0ea34a26b862fcb5ed70b6c226a
Brian Wang <bwbrianwang1018@gmail.com>
2022-09-21 15:29:39 -0700

    Add color to grid on mouse over

--- script.js
+++ script.js
@@ -6,10 +6,28 @@ const GRID_HEIGHT = 16;
 // Append it to the container div
 const container = document.querySelector(".container");
 
-for (let i = 0; i < GRID_WIDTH; i++) {
-    for (let j = 0; j < GRID_HEIGHT; j++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        container.appendChild(square);
+function createGrid() {
+    for (let i = 0; i < GRID_WIDTH; i++) {
+        for (let j = 0; j < GRID_HEIGHT; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            container.appendChild(square);
+        }
     }
 }
+
+function draw() {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+            square.classList.add("color");
+        });
+    });
+}
+
+function main() {
+    createGrid();
+    draw();
+}
+
+main();
